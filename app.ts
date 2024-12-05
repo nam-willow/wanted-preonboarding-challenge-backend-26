50ae0c2d5453475e12c28cb1dd131094a0916dd4
nam-willow <jnam63213@gmail.com>
2024-12-06 01:18:37 +0900

    update 결제버튼 및 view 관련

--- app.ts
+++ app.ts
@@ -1,13 +1,20 @@
 import express, { Request, Response } from "express";
+import routes from "./src/controllers/index";
 const handlebars = require("express-handlebars");
 const app = express();
 
 // 미들웨어 설정
 app.engine("handlebars", handlebars.engine()); // 템플릿 엔진으로 핸들바 등록
 app.set("view engine", "handlebars"); // 웹페이지 로드 시 사용할 템플릿 엔진 설정
-app.set("views", __dirname + "/views"); // 뷰 디랙터리를 views로 설정
+app.set("views", __dirname + "/src/views"); // 뷰 디랙터리를 views로 설정
+app.use(routes)
 
 // 라우터 설정
 app.get("/", (req: Request, res: Response) => {
-  // 컨트롤러로 넘어가는 내용 추가하기
+    res.render("layouts/main", { title: "안녕하세요", message: "만나서 반갑습니다." });
+});
+
+// 서버 시작
+app.listen(3000, () => {
+  console.log("서버가 3000번 포트에서 시작되었습니다.");
 });
