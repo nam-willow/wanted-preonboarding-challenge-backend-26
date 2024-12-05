08c7c6e1dfd1ea905f8c71f01856c034c5512a4d
nam-willow <jnam63213@gmail.com>
2024-12-05 22:39:51 +0900

    delete 테스트 파일 삭제

--- code3-1-ok-server.ts
+++
@@ -1,7 +0,0 @@
-// const http = require("http");
-// const server = http.createServer((req: any, res: any) => {
-//   res.setHeader("Content-Type", "text/html"); // 응답 헤더 설정
-//   res.end("OK"); // ok 를 응답하고 종료
-// });
-
-// server.listen("3000", () => console.log("ok 서버 시작!")); // 접속 대기
