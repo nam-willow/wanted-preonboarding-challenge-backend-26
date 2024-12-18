65b684bb43ce806b626628a5abaa7160eaa1571a
nam-willow <jnam63213@gmail.com>
2024-12-19 00:27:02 +0900

    update prettier: db.test.ts

--- src/test/db.test.ts
+++ src/test/db.test.ts
@@ -1,17 +1,17 @@
-import { pool } from '../database/mysql';
+import { pool } from "../../util/database/mysql";
 
-describe('Database Connection', () => {
-  it('should connect to the database and retrieve databases', async () => {
+describe("Database Connection", () => {
+  it("should connect to the database and retrieve databases", async () => {
     const connection = await pool.getConnection();
-    const [rows] = await connection.query('SHOW DATABASES');
-    
+    const [rows] = await connection.query("SHOW DATABASES");
+
     await connection.release();
     // rows의 길이를 확인
     const databases = Array.isArray(rows) ? rows : [rows]; // rows를 배열로 변환
     expect(databases.length).toBeGreaterThan(0); // 변경된 코드
   });
 
-  afterAll(async() => {
+  afterAll(async () => {
     await pool.end();
-  } )
+  });
 });
