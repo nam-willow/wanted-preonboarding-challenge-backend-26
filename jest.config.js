dec05d16d96810353334db675d796ba5d8b279e2
nam-willow <jnam63213@gmail.com>
2024-12-20 03:04:01 +0900

    update jets.config: orm test

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 // npm test 또는 npx jest 명령어 치면 실행되는 파일.
 
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['**/test/**/*.ts'], // test 디렉토리 내의 모든 .ts 파일을 테스트로 인식
+  preset: "ts-jest",
+  testEnvironment: "node",
+  setupFilesAfterEnv: ["<rootDir>/src/test/setup.ts"], // setup.ts 파일 추가
+  testMatch: ["**/src/test/**/*.test.ts"], // 테스트 파일 경로 설정
 };
