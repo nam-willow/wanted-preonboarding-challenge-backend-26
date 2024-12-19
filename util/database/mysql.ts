8f686536fdec209a28fd9b2da37e4bb884debeb6
nam-willow <jnam63213@gmail.com>
2024-12-20 03:17:29 +0900

    update mysql.ts: Products 엔티티 등록

--- util/database/mysql.ts
+++ util/database/mysql.ts
@@ -2,6 +2,7 @@ import "reflect-metadata"; // reflect-metadata는 TypeORM에서 필수
 import { DataSource } from "typeorm";
 import dotenv from "dotenv";
 import { User } from "../../src/entity/user.entity"; // 엔티티 임포트
+import { Products } from "../../src/entity/product.entity"; // 엔티티 임포트
 
 dotenv.config();
 
@@ -14,5 +15,5 @@ export const AppDataSource = new DataSource({
   database: process.env.DB_DATABASE || "",
   synchronize: false, // 개발 중에만 true (자동 테이블 생성)
   logging: false,
-  entities: [User], // 엔티티 등록
+  entities: [User, Products], // 엔티티 등록
 });
